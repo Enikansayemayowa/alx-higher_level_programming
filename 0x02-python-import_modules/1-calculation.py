fa942ca1d3a7975e6d35755593b26e800df914c6
Enikansayemayowa <enikansayemayowa405@gmail.com>
2023-08-10 11:04:38 -0700

    adding .format to 1-calculationn.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,11 +3,7 @@ if __name__ == "__main__":
     a = 10
     b = 5
     import calculator_1 as data
-    result = data.add(a, b)
-    print(f"{a} + {b} = {result}")
-    result = data.sub(a, b)
-    print(f"{a} - {b} = {result}")
-    result = data.mul(a, b)
-    print(f"{a} * {b} = {result}")
-    result = data.div(a, b)
-    print(f"{a} / {b} = {result}")
+    print("{} + {} = {}".format(a, b, data.add(a, b)))
+    print("{} - {} = {}".format(a, b, data.sub(a, b)))
+    print("{} * {} = {}".format(a, b, data.mul(a, b)))
+    print("{} / {} = {}".format(a, b, data.div(a, b)))
