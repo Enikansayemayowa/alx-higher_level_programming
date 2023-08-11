c5be74951d718de77dc2503dc423641eee5a42a7
Enikansayemayowa <enikansayemayowa405@gmail.com>
2023-08-11 13:18:03 -0700

    checking

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,11 @@
-#!/usr/bin/env python3
-if __name__ == "__main__":
-    import sys
-    args = sys.argv[1:]
-    total = sum(int(arg) for arg in args)
+#!/usr/bin/python3
 
+import sys
+
+def print_addition_of_args():
+    """Print the addition of all arguments."""
+    total = sum(int(arg) for arg in sys.argv[1:])
     print(total)
+    
+if __name__ == "__main__":
+    print_addition_of_args()
