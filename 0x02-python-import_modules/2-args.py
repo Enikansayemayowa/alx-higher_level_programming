3ed2c5f3767204b42749da46b02ac6b3603fc123
Enikansayemayowa <enikansayemayowa405@gmail.com>
2023-08-11 10:57:24 -0700

    check if error is fixed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,10 +5,7 @@ if __name__ == "__main__":
     num_arg = len(sys.argv) - 1
     if num_arg == 0:
         print("0 arguments.")
-    elif num_arg == 1:
-        print("1 argument:")
-        print("{} arguments:".format(num_arg))
-    else:
+    elif num_arg >= 1:
         print("{} arguments:".format(num_arg))
         for j in range(num_arg):
             print("{}: {}".format(j + 1, sys.argv[j + 1]))
