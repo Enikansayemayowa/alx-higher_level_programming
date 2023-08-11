9bba0cfac8564109c76a76b02ccf23a3b068f035
Enikansayemayowa <enikansayemayowa405@gmail.com>
2023-08-11 10:29:09 -0700

    change code format!

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,11 +3,10 @@ import sys
 if __name__ == "__main__":
     num_args = len(sys.argv) - 1
     if num_args == 0:
-        print("Number of argument(s): 0.")
-        print(".")
+        print("0 arguments.")
     else:
         if num_args == 1:
-            print("Number of argument(s): 1:")
+            print("1 argument:")
         else:
             print("{} arguments:".format(num_args))
 
