8e2d5c7be838a3e531cc71d192b68487134c3506
Enikansayemayowa <enikansayemayowa405@gmail.com>
2023-08-14 18:36:11 -0700

    checking if error persist

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,10 +6,13 @@ if __name__ == "__main__":
     if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-        option = {"+": add, "-": sub, "*": mul, "/": div}
-        if sys.argv[2] not in list(option.keys()):
-            print("Unknown operator. Available operators: +, -, * and /")
-            sys.exit(1)
-            a = int(sys.argv[1])
-            b = int(sys.argv[3])
-            print("{} {} {} = {}".format(a, sys.argv[2], b, option[sys.argv[2]](a, b)))
+
+    option = {"+": add, "-": sub, "*": mul, "/": div}
+
+    if sys.argv[2] not in list(option.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    print("{} {} {} = {}".format(a, sys.argv[2], b, option[sys.argv[2]](a, b)))
