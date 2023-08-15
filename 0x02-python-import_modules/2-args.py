39c02f559dd68dc1b1bdeee0a09aed14db1c486d
Enikansayemayowa <enikansayemayowa405@gmail.com>
2023-08-14 18:25:19 -0700

    modify the codes

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
+import sys
+
 if __name__ == "__main__":
-    import sys
     num_arg = len(sys.argv) - 1
     if num_arg == 0:
         print("0 arguments.")
-    elif num_arg >= 1:
-        print("{} arguments:".format(num_arg))
+    else:
+        print("{} arguments:".format(num_arg, "s" if num_args != 1 else ""))
         for j in range(num_arg):
             print("{}: {}".format(j + 1, sys.argv[j + 1]))
