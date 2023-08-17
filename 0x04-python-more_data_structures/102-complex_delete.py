e0f33ffb957f5dc1c037b821a8fd93e7492b5089
Enikansayemayowa <enikansayemayowa405@gmail.com>
2023-08-16 18:26:37 -0700

    changed function

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    new_value = list(a_dictionary.keys())
-    for k in new_value:
-        if value == a_dictionary.get(k):
-            del a_dictionary[k]
+    keys_to_delete = []
+
+    for k, v in a_dictionary.items():
+        if v == value:
+            keys_to_delete.append(k)
+
+    for k in keys_to_delete:
+        del a_dictionary[k]
 
     return a_dictionary
