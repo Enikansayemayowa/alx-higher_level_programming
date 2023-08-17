5adb65e04526191cd642d694128845d71669b8c0
Enikansayemayowa <enikansayemayowa405@gmail.com>
2023-08-16 18:17:17 -0700

    fixing the typo k in place of key

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -3,6 +3,6 @@ def complex_delete(a_dictionary, value):
     new_value = list(a_dictionary.keys())
     for k in new_value:
         if value == a_dictionary.get(k):
-            del a_dictionary[key]
+            del a_dictionary[k]
 
     return a_dictionary
