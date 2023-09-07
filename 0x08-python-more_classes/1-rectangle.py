6b2cee9517acefbc789893222de1bdca117d6b8f
Enikansayemayowa <enikansayemayowa405@gmail.com>
2023-09-06 18:45:12 -0700

    added comment/doc

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
+"""Defines a Rectangle class."""
 class Rectangle:
+    """Defines a Rectangle class."""
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
 
-    
+    """declaring the getter for width"""
     @property
     def width(self):
         return self.__width
 
+"""width setter"""
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
@@ -18,10 +21,12 @@ class Rectangle:
         else:
             self.__width = value
 
+    """declaring the getter for height"""
     @property
     def height(self):
         return self.__height
 
+    """setter for height"""
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
