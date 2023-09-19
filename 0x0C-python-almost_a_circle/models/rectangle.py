b588a8ad58399ef0c1d8928949f2dbbfc15a2f94
Enikansayemayowa <enikansayemayowa405@gmail.com>
2023-09-18 18:44:08 -0700

    is it working?

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,7 +6,6 @@ from base import Base
 class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         """instance attributes, each with its own public"""
-
         super().__init__(id)
         self.width = width
         self.height = height
@@ -18,29 +17,46 @@ class Rectangle(Base):
         return self.__width
 
     @width.setter
-    def width(self, width):
-        self.__width = width
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("Width must be an integer")
+        if value <= 0:
+            raise ValueError("Width must be a positive integer")
+        self.__width = value
 
     @property
     def height(self):
         return self.__height
 
     @height.setter
-    def height(self, height):
-        self.__height = height
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("Height must be an integer")
+        if value <= 0:
+            raise ValueError("Height must be a positive integer")
+        self.__height = value
 
     @property
     def x(self):
         return self.__x
 
     @x.setter
-    def x(self, x):
-        self.__x = x
+    def x(self, value):
+        if not isinstance(value, int):
+            raise TypeError("X must be an integer")
+        if value < 0:
+            raise ValueError("X cannot be negative")
+        self.__x = value
 
     @property
     def y(self):
         return self.__y
 
     @y.setter
-    def y(self, y):
-        self.__y = y
+    def y(self, value):
+        if not isinstance(value, int):
+            raise TypeError("Y must be an integer")
+        if value < 0:
+            raise ValueError("Y cannot be negative")
+        self.__y = value
+
